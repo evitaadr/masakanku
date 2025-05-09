84516d14e2b2367e4c6c79ce19b2b71b81f8f584
evita <evitadwi954@gmail.com>
2025-05-09 14:48:16 +0700

    layout

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Restaurant')</title>
-    <!-- Bootstrap CSS -->
+    <!-- Bootstrap CSS ini -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
